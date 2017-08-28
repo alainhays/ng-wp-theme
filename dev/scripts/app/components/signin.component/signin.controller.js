6ae879a96839be8eed2c6935b473ba25a0ab03ac
Denis Dudnik <denis@iqq.se>
2017-08-28 12:18:56 +0300

    Removed login logic for start wp-passport strategy

--- dev/scripts/app/components/signin.component/signin.controller.js
+++ dev/scripts/app/components/signin.component/signin.controller.js
@@ -3,33 +3,5 @@ class SignInController {
     this.message = 'SignInController';
   };
 
-  // $onInit() {
-  //   this.newTodo = {
-  //     title: '',
-  //     selected: false
-  //   };
-  //   this.todos = [];
-  //   this.todoService.getTodos().then(response => {
-  //     console.log(response);
-  //     this.todos = response
-  //   });
-  // }
-  // $onChanges(changes) {
-  //   if (changes.todoData) {
-  //     this.todos = Object.assign({}, this.todoData);
-  //   }
-  // }
-  // addTodo({ todo }) {
-  //   if (!todo) return;
-  //   this.todos.unshift(todo);
-  //   console.log(this.todos);
-  //   this.newTodo = {
-  //     title: '',
-  //     selected: false
-  //   };
-  // }
 }
-
-// TodoController.$inject = ['TodoService'];
-
 export default SignInController;
--- dev/scripts/app/components/signin.component/signin.service.js
+++ dev/scripts/app/components/signin.component/signin.service.js
@@ -4,20 +4,6 @@ class SignInService {
     this.$http = $http;
   };
 
-  loginUser(username, password) {
-    return this.$http.get(globals.ajaxurl, {
-      params: {
-        action: 'loginFn',
-        username: username,
-        password: password
-      }
-    }).then( (response) => {
-        return response.data;
-      })
-      .catch( (error) => {
-        console.error(error);
-      });
-  };
 };
 
 export default SignInService;
